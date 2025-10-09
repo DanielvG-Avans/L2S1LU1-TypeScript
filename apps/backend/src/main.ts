c93808f20ff89d4636c909c7c7fd1240e8f30a45
Daniel van Ginneken <dcjvanginneken@gmail.com>
2025-10-09 12:07:12 +0200

    chore: move apps to seperate folder

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,11 +6,11 @@ const logger = new Logger("Bootstrap");
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap()
   .then(() => {
-    logger.log("Application is running on port " + (process.env.PORT || 3000));
+    logger.log("Application is running on port " + (process.env.PORT || 3001));
   })
   .catch((err) => {
     logger.error("Error during app bootstrap:", err);
