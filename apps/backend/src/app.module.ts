151e3c805f198a64689a63ae20c22bd6a8ae7890
Daniel van Ginneken <dcjvanginneken@gmail.com>
2025-10-12 19:39:53 +0200

    feat: add AuthController to AppModule for authentication routing
    Signed-off-by: Daniel van Ginneken <dcjvanginneken@gmail.com>

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 import { InfrastructureModule } from "./infrastructure/infrastructure.module";
 import { AuthService } from "./application/auth.service";
+import { AuthController } from "./interfaces/controllers/auth.controller";
 
 @Module({
   imports: [InfrastructureModule],
   providers: [AuthService],
+  controllers: [AuthController],
 })
 export class AppModule {}
