7041ea58e751f5a5e8e9ec8c5496a5b72fea59d6
Daniel van Ginneken <dcjvanginneken@gmail.com>
2025-10-13 21:59:40 +0200

    feat: implement AuthGuard for JWT authentication

---
+++ apps/backend/src/interfaces/guards/auth.guard.ts
@@ -0,0 +1,32 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import { jwtSecret } from "../../constants";
+import { JwtService } from "@nestjs/jwt";
+import { Request } from "express";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request: Request = context.switchToHttp().getRequest<Request>();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    try {
+      const payload: { [key: string]: any } = await this.jwtService.verifyAsync(token, {
+        secret: jwtSecret,
+      });
+
+      request["user"] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(" ") ?? [];
+    return type === "Bearer" ? token : undefined;
+  }
+}
