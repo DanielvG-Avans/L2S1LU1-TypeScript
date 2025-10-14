78e373667f24aef7da111e4c15beebb51b51e89e
Daniel van Ginneken <dcjvanginneken@gmail.com>
2025-10-14 15:17:28 +0200

    feat: add logout functionality to header component

--- apps/frontend/src/layouts/Layout.tsx
+++ apps/frontend/src/layouts/Layout.tsx
@@ -7,14 +7,19 @@ import { useUser } from "../hooks/useUser";
 export const Layout = (): React.ReactNode | null => {
   const [user, setUser] = useState<User | null>(null);
   const fetchedUser = useUser();
-
   useEffect(() => {
     setUser(fetchedUser);
   }, [fetchedUser]);
 
+  const onLogout = () => {
+    setUser(null);
+    cookieStore.delete("ACCESSTOKEN");
+    window.location.replace("/auth/login");
+  };
+
   return user ? (
     <>
-      <Header user={user} />
+      <Header user={user} onLogout={onLogout} />
       <main>
         <Outlet />
       </main>
